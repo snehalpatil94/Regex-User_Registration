88f039a76478c340208bc2072530ee342a053e2a
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-26 20:47:21 +0530

    Validation of Last Name

--- src/com/bridgelabz/regex/RegexMain.java
+++ src/com/bridgelabz/regex/RegexMain.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regex;
 import java.util.Scanner;
 
 /**
- * valid First Name - First name starts with Cap and has minimum 3 characters
+ * Valid Last Name - Last name starts with Cap and has minimum 3 characters
  * 
  * @author : Snehal Patil
  *
@@ -19,13 +19,20 @@ public class RegexMain {
 		// Create object of ValidateUserRegistration class
 		ValidateUserRegistration validate = new ValidateUserRegistration();
 
+		// Method call to validate user first name
 		validate.validateFirstName("Snehal");
-		
 		validate.validateFirstName("snehal");
-		
+
 		System.out.println("Enter First name: ");
 		String firstName = sc.nextLine();
-		// method call to validate user first name
 		validate.validateFirstName(firstName);
+
+		// Method call to validate user last name
+		validate.validateLastName("Patil");
+		validate.validateLastName("patil");
+
+		System.out.println("Enter Last name: ");
+		String lastName = sc.nextLine();
+		validate.validateFirstName(lastName);
 	}
 }
--- src/com/bridgelabz/regex/ValidateUserRegistration.java
+++ src/com/bridgelabz/regex/ValidateUserRegistration.java
@@ -16,4 +16,19 @@ public class ValidateUserRegistration {
 			return false;
 		}
 	}
+	
+	// Method to validate user last name
+	public boolean validateLastName(String name) {
+		String pattern = "^[A-Z]{1}[a-zA-Z]{2,}$";
+
+		if (name.matches(pattern)) {
+			System.out.println("'" + name + "'" + " is Valid Last Name !");
+			System.out.println("------------------------------------");
+			return true;
+		} else {
+			System.out.println("'" + name + "'" + " is Invalid Last Name !");
+			System.out.println("------------------------------------");
+			return false;
+		}
+	}
 }
